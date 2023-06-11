c191ca9169fb6b59c2ea6d97623ab870fe3e4333
rxn3202 <126435652+rxn3202@users.noreply.github.com>
2023-06-10 21:26:01 -0700

    function for sql in server.js

--- server.js
+++ server.js
@@ -1,4 +1,28 @@
-const inquirer = require('inquirer');
-const db = require('./db/connection');
+const mysql = require('mysql2');
 require('dotenv').config();
 
+// Create a connection pool
+const pool = mysql.createPool({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PW,
+  database: process.env.DB_NAME,
+  waitForConnections: true,
+  connectionLimit: 10,
+  queueLimit: 0,
+});
+
+// Function to execute SQL queries
+function executeQuery(sql, params) {
+  return new Promise((resolve, reject) => {
+    pool.query(sql, params, (error, results) => {
+      if (error) {
+        reject(error);
+      } else {
+        resolve(results);
+      }
+    });
+  });
+}
+
+promptAction();
