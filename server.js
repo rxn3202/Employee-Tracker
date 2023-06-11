93d639a831f058272a6c0c326d92f6bd41da3224
rxn3202 <126435652+rxn3202@users.noreply.github.com>
2023-06-10 22:02:55 -0700

    completed prompt for initial action

--- server.js
+++ server.js
@@ -26,40 +26,55 @@ function executeQuery(sql, params) {
   });
 }
 
+// Prompt for the initial action
 function promptAction() {
     inquirer
-        .prompt([
-            {
-                type: 'list',
-                name: 'action',
-                message: 'What would you like to do?',
-                choices: [
-                    'View all departments',
-                    'View all roles',
-                    'Veiw all employees',
-                    'Add a deparment',
-                    'Add a role',
-                    'Add an employee',
-                    'Update an employee role',
-                    'Exit',
-                ],
-            },
-        ])
-        .then((answers) => {
-            switch (answers.action) {
-                case 'View all deparments':
-                    viewDepartments();
-                    break;
-                case ''
-                case ''
-                case ''
-                case ''
-                case ''
-                case ''
-                case ''    
-            }
-        })
-}
+      .prompt([
+        {
+          type: 'list',
+          name: 'action',
+          message: 'What would you like to do?',
+          choices: [
+            'View all departments',
+            'View all roles',
+            'View all employees',
+            'Add a department',
+            'Add a role',
+            'Add an employee',
+            'Update an employee role',
+            'Exit',
+          ],
+        },
+      ])
+      .then((answers) => {
+        switch (answers.action) {
+          case 'View all departments':
+            viewDepartments();
+            break;
+          case 'View all roles':
+            viewRoles();
+            break;
+          case 'View all employees':
+            viewEmployees();
+            break;
+          case 'Add a department':
+            addDepartment();
+            break;
+          case 'Add a role':
+            addRole();
+            break;
+          case 'Add an employee':
+            addEmployee();
+            break;
+          case 'Update an employee role':
+            updateEmployeeRole();
+            break;
+          case 'Exit':
+            console.log('Goodbye!');
+            process.exit();
+        }
+      });
+  }
 
 // View all departments
 function viewDepartments() {
