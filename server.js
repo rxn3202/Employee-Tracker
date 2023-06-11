8e9bd8372d05c730fac1d947dc97348c7fc6f9f5
rxn3202 <126435652+rxn3202@users.noreply.github.com>
2023-06-10 22:20:43 -0700

    function added to view all employees

--- server.js
+++ server.js
@@ -104,6 +104,25 @@ function viewDepartments() {
       });
   }
 
+  // View all employees
+function viewEmployees() {
+    const sql = `SELECT employee.id, employee.first_name, employee.last_name, 
+      role.title, department.name AS department, role.salary,
+      CONCAT(manager.first_name, ' ', manager.last_name) AS manager
+      FROM employee
+      JOIN role ON employee.role_id = role.id
+      JOIN department ON role.department_id = department.id
+      LEFT JOIN employee manager ON employee.manager_id = manager.id`;
+    executeQuery(sql)
+      .then((results) => {
+        console.table(results);
+        promptAction();
+      })
+      .catch((error) => {
+        console.log('An error occurred while retrieving employees:', error);
+        promptAction();
+      });
+  }
 
 
 promptAction();
